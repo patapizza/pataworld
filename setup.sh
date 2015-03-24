95f114d94a2014f7c14ae323cf722e0907d0d9d1
patapizza <julien@wit.ai>
2015-03-23 21:18:39 -0700

    adding paredit and surround

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-[[ ! -x ./setup.sh ]] && echo "Couldn't find executable ./setup.sh" && exit 1
-
-echo "Copying .vimrc and .zshrc..."
+echo "Copying ~/.vimrc, ~/.zshrc and ~/.vim..."
 [[ -f ~/.vimrc ]] && mv ~/.vimrc ~/.vimrc.backup
 [[ -f ~/.zshrc ]] && mv ~/.zshrc ~/.zshrc.backup
-cp .vimrc ~/.vimrc
-cp .zshrc ~/.zshrc
-rm -rf .
+[[ -d ~/.vim ]] && mv ~/.vim ~/.vim.backup
+
+mv .vimrc ~/.vimrc
+mv .zshrc ~/.zshrc
 
 # vim
 ## pathogen
@@ -22,9 +21,11 @@ echo "[vim] Installing solarized..."
 git clone https://github.com/altercation/vim-colors-solarized.git
 
 ## clojure
-echo "[vim] Installing clojure..."
+echo "[vim] Installing clojure plugins..."
 git clone https://github.com/tpope/vim-fireplace.git
 git clone https://github.com/guns/vim-clojure-static.git
 git clone https://github.com/guns/vim-clojure-highlight.git
+git clone https://github.com/vim-scripts/paredit.vim.git
+git clone https://github.com/tpope/vim-surround.git
 
 echo "done!"
