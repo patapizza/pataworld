6089d24b522e2488c365d9d7963d5e3d33060f50
patapizza <julien@wit.ai>
2015-06-19 01:37:43 -0700

    80col + theme toggle + cleaning up

--- init.el
+++ init.el
@@ -1,5 +1,4 @@
-(defalias 'vi 'evil-mode)
-
+;; Setup
 (defun ensure-package (package-name)
   (unless (package-installed-p package-name)
     (package-refresh-contents)
@@ -11,19 +10,21 @@
     (ensure-packages (cdr packages))))
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 (ensure-packages '(auto-complete
-                    cider
-                    clojure-mode
-                    company
-                    evil
-                    evil-paredit
-                    ido-ubiquitous
-                    paredit
-                    projectile
-                    smartparens))
+		   cider
+		   clojure-mode
+		   company
+		   evil
+		   evil-paredit
+		   ido-ubiquitous
+		   paredit
+		   projectile
+		   smartparens))
 
 (require 'auto-complete)
 (require 'cider)
@@ -32,35 +33,52 @@
 (require 'ido-ubiquitous)
 (require 'projectile)
 (require 'smartparens-config)
+(require 'whitespace)
 
+;; Custom functions
 (defun bind-evil (keyseq symbol &optional state-map)
   (define-key (or state-map evil-normal-state-map) (kbd keyseq) symbol))
-
-(defun change-parenthesis-at-open (wrap-fn)
-  (funcall wrap-fn)
-  (forward-char)
-  (paredit-splice-sexp))
-
-(defun change-parenthesis-at-close (wrap-fn)
-  (paredit-backward-up)
-  (change-parenthesis-at-open wrap-fn))
-
-(defun open-parenthesisp (c)
-  (or (string= "(" c) (string= "[" c) (string= "{" c)))
-
-(defun close-parenthesisp (c)
-  (or (string= ")" c) (string= "]" c) (string= "}" c)))
 
 (defun change-parenthesis ()
   (interactive)
   (save-excursion
-    (let ((c (char-to-string (following-char)))
-          (wrap-fn (cond ((= 40 last-command-event) 'paredit-wrap-round) ; "("
-                         ((= 91 last-command-event) 'paredit-wrap-square) ; "["
-                         ((= 123 last-command-event) 'paredit-wrap-curly)))) ; "{"
+    (let* ((c (char-to-string (following-char)))
+	   (open-paren? (or (string= "(" c) (string= "[" c) (string= "{" c)))
+	   (close-paren? (or (string= ")" c) (string= "]" c) (string= "}" c)))
+	   (change-paren-left (lambda (f)
+				(funcall f)
+				(forward-char)
+				(paredit-splice-sexp)))
+	   (wrap-fn (pcase last-command-event
+		      (40 'paredit-wrap-round) ; "("
+		      (91 'paredit-wrap-square) ; "["
+		      (123 'paredit-wrap-curly)))) ; "{"
       (when wrap-fn
-        (cond ((open-parenthesisp c) (change-parenthesis-at-open wrap-fn))
-              ((close-parenthesisp c) (change-parenthesis-at-close wrap-fn)))))))
+	(cond (open-paren? (funcall change-paren-left wrap-fn))
+	      (close-paren? (paredit-backward-up)
+			    (funcall change-paren-left wrap-fn)))))))
+
+(defvar loaded-theme)
+(defun toggle-theme (theme)
+  (set-frame-parameter nil 'background-mode theme)
+  (set-terminal-parameter nil 'background-mode theme)
+  (enable-theme 'solarized)
+  (setq loaded-theme theme))
+
+;; Hooks
+(defun cider-hook ()
+  (cider-turn-on-eldoc-mode)
+  (company-mode 1))
+
+(defun clojure-hook ()
+  (bind-evil "M-." 'cider-find-var)
+  (bind-evil ",cc" 'cider-connect)
+  (bind-evil ",d" 'cider-doc)
+
+  (define-clojure-indent
+    (fact 'defun)
+    (fnk 'defun)
+    (go-try 'defun)))
 
 (defun lisp-hook ()
   (local-set-key (kbd "RET") 'newline-and-indent)
@@ -75,20 +93,16 @@
   (dolist (parenthesis '("(" "[" "{"))
     (bind-evil (concat ",c" parenthesis) 'change-parenthesis)))
 
-(defun clojure-hook ()
-  (bind-evil "M-." 'cider-find-var)
-  (bind-evil ",cc" 'cider-connect)
-  (bind-evil ",d" 'cider-doc)
+(add-hook 'cider-mode-hook 'cider-hook)
+(add-hook 'clojure-mode-hook 'lisp-hook)
+(add-hook 'clojure-mode-hook 'clojure-hook)
+(add-hook 'emacs-lisp-mode-hook 'lisp-hook)
+(add-hook 'prog-mode-hook 'whitespace-mode)
 
-  (define-clojure-indent
-    (fact 'defun)
-    (fnk 'defun)
-    (go-try 'defun)))
+;; Autocomplete
+(global-auto-complete-mode t)
 
-(defun cider-hook ()
-  (cider-turn-on-eldoc-mode)
-  (company-mode 1))
-
+;; Cider
 (setq cider-auto-select-error-buffer t
       cider-popup-stacktraces t
       cider-repl-history-file "/tmp/cider-repl-history"
@@ -101,52 +115,58 @@
 (evil-mode t)
 (bind-evil "\\b" 'projectile-switch-to-buffer)
 (bind-evil "\\p" 'projectile-find-file)
-(bind-evil "C-k" (lambda ()
-                   (interactive)
-                   (evil-scroll-up nil)))
-(bind-evil "C-j" (lambda ()
-                   (interactive)
-                   (evil-scroll-down nil)))
-(bind-evil "C-z" 'suspend-frame)
+(bind-evil "\\s" (lambda ()
+		   (interactive)
+		   (toggle-theme (if (eq 'dark loaded-theme) 'light 'dark))))
 (bind-evil "-" 'evil-window-next)
+(bind-evil "C-j" (lambda ()
+		   (interactive)
+		   (evil-scroll-down nil)))
+(bind-evil "C-k" (lambda ()
+		   (interactive)
+		   (evil-scroll-up nil)))
+(bind-evil "C-z" 'suspend-frame)
 (bind-evil "gc" 'comment-or-uncomment-region evil-visual-state-map)
 (bind-evil "gcc" (lambda ()
-                   (interactive)
-                   (comment-or-uncomment-region (line-beginning-position)
-                                                (line-end-position))))
+		   (interactive)
+		   (comment-or-uncomment-region (line-beginning-position)
+						(line-end-position))))
 
+;; Ido
+(setq
+ ido-enable-flex-matching t
+ ido-everywhere t)
 
-(global-set-key (kbd "RET") 'newline-and-indent)
+(ido-ubiquitous-mode 1)
 
 ;; Smartparens
 (sp-with-modes sp--lisp-modes
-               (sp-local-pair "(" nil :bind "M-("))
+  (sp-local-pair "(" nil :bind "M-("))
 
-;; Autocomplete
-(global-auto-complete-mode t)
+(custom-set-variables
+ '(sp-base-key-bindings 'paredit)
+ '(sp-autoskip-closing-pair 'always))
 
 ;; Solarized
 (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
 (load-theme 'solarized t)
-(set-frame-parameter nil 'background-mode 'dark)
-(set-terminal-parameter nil 'background-mode 'dark)
-(enable-theme 'solarized)
+(toggle-theme 'light)
 
-
-(add-hook 'emacs-lisp-mode-hook 'lisp-hook)
-(add-hook 'clojure-mode-hook 'lisp-hook)
-(add-hook 'clojure-mode-hook 'clojure-hook)
-(add-hook 'cider-mode-hook 'cider-hook)
-
-(custom-set-variables
-  '(sp-base-key-bindings 'paredit)
-  '(sp-autoskip-closing-pair 'always))
-
+;; Whitespace
 (setq
-  ack-default-directory-function '(lambda (&rest args) (projectile-project-root))
-  ido-enable-flex-matching t
-  ido-everywhere t
-  tab-width 2)
+ whitespace-action '(auto-cleanup)
+ whitespace-style '(empty face tabs indentation lines-tail space-after-tab
+		    space-before-tab trailing)
+ whitespace-line-column 81)
+
+;; Global config
+(setq
+ ack-default-directory-function '(lambda (&rest args) (projectile-project-root))
+ column-number-mode t
+ indent-tabs-mode nil
+ line-number-mode t
+ tab-width 2)
 
 (global-linum-mode t)
-(ido-ubiquitous-mode 1)
+(global-set-key (kbd "RET") 'newline-and-indent)
+(menu-bar-mode -1)
